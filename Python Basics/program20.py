b085f7697846a876478a91ea26b874e8a56acd8e
Devendra Patil <121740109+patildevendra-0@users.noreply.github.com>
2023-02-24 20:27:49 +0530

    Add files via upload

---
+++ Python Basics/program20.py
@@ -0,0 +1,12 @@
+
+import Arithematic as ART
+
+def main():
+
+    print("Enter the number :")
+    No = int(input())
+
+    ART.DisplayFactors(No)
+if __name__=="__main__":
+    main()    
+
---
+++ Python Basics/program28.py
@@ -0,0 +1,12 @@
+print("Demonstration of tuple...")
+
+Data1 = (10,20,30,40,10,10,20)
+Data2 = (10,10.23,True,"hello")
+
+print(Data1)
+print(Data2)
+
+print(Data1[3])
+
+Data1.append()
+print(Data1)
